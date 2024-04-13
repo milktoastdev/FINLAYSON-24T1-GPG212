58ec577c68eb78f881fd449ccacec44602e99f2f
MILKY <finlayson.ariana@gmail.com>
2024-04-13 19:02:46 +1000

    || File Systems & Custom Editors || * Created functions for saving and loading * Added placeholder sets and gets for playerprefs, 'name' and 'colour' * These have default values * Created an editor script for testing save/load functions

---
+++ Assets/Scripts/Editor/SaveManagerEditor.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// <para>Draws editor buttons for testing save/load functions.</para>
+/// <para>This isn't an editor window because it's only applicable to one component.</para>
+/// <para>Editor for child classes is false because there will be no child classes.</para>
+/// </summary>
+[CustomEditor(typeof(SaveManager), false)]
+public class SaveManagerEditor : Editor
+{
+    /// <summary>
+    /// <para>Draws the editor in the inspector.</para>
+    /// <para>Making it an override void allows us to customise our editor.</para>
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        // Draws the default editor in the inspector.
+        base.OnInspectorGUI();
+        
+        // Begins laying out buttons horizontally.
+        GUILayout.BeginHorizontal();
+        
+        if (GUILayout.Button("Save"))
+        {
+            // 'target' is a magical word that tells the editor where to find the original component so you can access it's functions.
+            
+            /*
+            SaveManager saveManager;
+            saveManager = target as SaveManager;
+            saveManager?.SavePlayerPrefs();
+            */
+            
+            // Cleaner line using shortcuts.
+            (target as SaveManager)?.SavePlayerPrefs();
+        }
+
+        if (GUILayout.Button("Load"))
+        {
+            /*
+            SaveManager saveManager;
+            saveManager = target as SaveManager;
+            saveManager?.LoadPlayerPrefs();
+            */
+            
+            // Cleaner line using shortcuts.
+            (target as SaveManager)?.LoadPlayerPrefs();
+        }
+        
+        // Ends laying out buttons horizontally.
+        GUILayout.EndHorizontal();
+    }
+}
--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -2,12 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Holds the functions for saving and loading.
+/// </summary>
 public class SaveManager : MonoBehaviour
 {
-    // MINIMUM
-    // Name --> field into menus
-    // Colour
-    // Save & Load on play
-    // other player settings
-    // volume etc
+    // JSON the fuck outta this!
+    
+    public string playerName;
+    public string playerColour;
+    // Volume settings
+    // Score / Highscore
+    // Input settings (keyboard/gamepad)
+    
+    /// <summary>
+    /// Saving data using player prefs
+    /// </summary>
+    public void SavePlayerPrefs()
+    {
+        // Editors work outside of play mode. That's scary.
+        if (Application.isPlaying)
+        {
+            // This should be chosen using an input field in a menu
+            PlayerPrefs.SetString("PlayerName",playerName);
+            PlayerPrefs.SetString("PlayerColour",playerColour);
+        }
+    }
+
+    /// <summary>
+    /// Loading data using player prefs
+    /// </summary>
+    public void LoadPlayerPrefs()
+    {
+        // Editors work outside of play mode. That's scary.
+        if (Application.isPlaying)
+        {
+            PlayerPrefs.GetString("PlayerName","PLAYER");
+            PlayerPrefs.GetString("PlayerColour","Grey");
+        }
+    }
 }

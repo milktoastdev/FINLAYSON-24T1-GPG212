02a03857b2ade8c28337164b3e69931441208fe7
MILKY <finlayson.ariana@gmail.com>
2024-04-17 00:02:53 +1000

    || Assets || * Adding simple assets * Installed DOTween

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class SaveManager : MonoBehaviour
 {
     // JSON the fuck outta this!
+    // File Systems --> Cam Doc
     
     public string playerName;
     public string playerColour;
@@ -24,6 +25,11 @@ public class SaveManager : MonoBehaviour
         if (Application.isPlaying)
         {
             // This should be chosen using an input field in a menu
+            // Using a dictionary, not just because its the only thing playerprefs accepts, but for a key!
+            // Arrays & lists look shit up using an index (number)
+            // Dictionaries look shit up by looking up the thing itself!
+            // Both are useful & have different purposes and usages
+            // In dictionaries, you have to declare the KEY, then the VALUE
             PlayerPrefs.SetString("PlayerName",playerName);
             PlayerPrefs.SetString("PlayerColour",playerColour);
         }

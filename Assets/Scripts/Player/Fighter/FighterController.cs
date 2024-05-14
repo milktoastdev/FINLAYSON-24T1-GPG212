0c5a52e3dc1b2bca531a973a62ad690c18ea2c07
MILKY <finlayson.ariana@gmail.com>
2024-05-14 13:52:32 +1000

    Cleaning up FighterController script.

--- Assets/Scripts/Player/Fighter/FighterController.cs
+++ Assets/Scripts/Player/Fighter/FighterController.cs
@@ -6,39 +6,54 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 /// <summary>
-/// This only cares about the player's input.
-/// Functions that actually force the fighter to move are located in the model.
+/// Purely player inputs for character movement.
 /// </summary>
 public class FighterController : MonoBehaviour
 {
-    public bool debug;
-
+    /// <summary>
+    /// Refers to the model of the character (root GO).
+    /// </summary>
     public FighterBase fighterBase;
 
-    public Rigidbody2D rigidbody2D;
+    // TODO: Move this to the model of the character.
+    /// <summary>
+    /// Character's rigidbody.
+    /// </summary>
+    public Rigidbody2D rb2D;
     
+    /// <summary>
+    /// Force to be applied to the character's rigidbody.
+    /// </summary>
     public float moveSpeed;
     
+    // TODO: Move these to the model of the character.
+    /// <summary>
+    /// Make it possible to lock the player's ability to move the character.
+    /// </summary>
     public bool canMove;
     public bool canJump;
     
-    // This will be replaced with action maps
-    // Mapped in inspector to arrow keys
+    // TODO: Replace with action map(s)
+    /// <summary>
+    /// Variables for player movement.
+    /// Assigned in inspector to arrow keys.
+    /// </summary>
     public KeyCode moveLeft;
     public KeyCode moveRight;
     public KeyCode moveJump;
     public KeyCode moveSquat;
     
+    /// <summary>
+    /// Enables players to move the character.
+    /// </summary>
     public void OnEnable()
     {
-        debug = true;
-        
         canMove = true;
         canJump = true;
     }
 
     /// <summary>
-    /// The inputs here call the functions within FighterBase that invoke their respective events.
+    /// Inputs here call the functions within FighterBase that invoke their respective movement events.
     /// </summary>
     public void FixedUpdate()
     {
@@ -55,19 +70,22 @@ public class FighterController : MonoBehaviour
         if (Input.GetKeyDown(moveJump) && canMove && canJump)
         {
             fighterBase.OnJumpEvent();
+            // TODO: Add an OnLandEvent so you can lock and unlock double-jumping or jumping at all.
         }
         
         if (Input.GetKey(moveSquat) && canMove)
         {
             fighterBase.OnSquatEvent();
+            // TODO: Lock left, right & jump movement while this is happening.
         }
     }
 
+    /// <summary>
+    /// Disables players from moving the character.
+    /// </summary>
     public void OnDisable()
     {
         canMove = false;
         canJump = false;
-
-        debug = false;
     }
 }

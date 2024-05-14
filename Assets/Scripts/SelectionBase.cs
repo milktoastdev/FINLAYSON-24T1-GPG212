7eefa9f024894deee0a6a81fed651a43450e3856
MILKY <finlayson.ariana@gmail.com>
2024-05-14 13:53:32 +1000

    Deleted SelectionBase script because I left no indication for what ut was being used for & it contained no code.

--- Assets/Scripts/SelectionBase.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Built into Unity for ease of object selection in scene view
-/// </summary>
-[SelectionBase]
-public class SelectionBase : MonoBehaviour
-{
-}
